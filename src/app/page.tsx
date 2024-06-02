deeb18be115880974c6b4ed4eae6c63c6513a808
manh tran duc <manhtranduc0202@gmail.com>
2024-06-02 09:16:55 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { manrope, roboto } from "@/components/fonts";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <main className={`font-primary`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+        ducimus. Voluptate quas, sint ullam eum vel ad, repellendus doloremque
+        porro ex, enim quidem doloribus asperiores aliquid. Sapiente, qui.
+        <h1 className={`font-secondary font-bold`}>Dolorum, harum.</h1>
+      </main>
+    </main>
+  );
+}
