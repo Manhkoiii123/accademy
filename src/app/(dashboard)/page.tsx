92bd0e8c036c591caa2a604a561dd1df78ea2372
manhtranduc <manhtranduc0202@gmail.com>
2024-06-13 22:17:44 +0700

    redeploy webhook

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,12 +5,6 @@ import createUser from "@/lib/actions/user.actions";
 import React from "react";
 
 const page = async () => {
-  const user = createUser({
-    clerkId: "1111",
-    name: "Nguyễn Văn A",
-    username: "nguyenvana",
-    email_address: "manh@email.com",
-  });
   return (
     <div>
       <Heading>Khám phá</Heading>
