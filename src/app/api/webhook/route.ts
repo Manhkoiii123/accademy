167aca5f1981ede4e556d9d9b1bddf14d405e504
manhtranduc <manhtranduc0202@gmail.com>
2024-06-13 22:50:09 +0700

    update webhook theo anh evondev

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,13 +1,12 @@
 import createUser from "@/lib/actions/user.actions";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
-
 export async function POST(req: Request) {
-  const svix_id = req.headers.get("svix-id") ?? "";
-  const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
-  const svix_signature = req.headers.get("svix-signature") ?? "";
-
+  const svix_id = headers().get("svix-id") ?? "";
+  const svix_timestamp = headers().get("svix-timestamp") ?? "";
+  const svix_signature = headers().get("svix-signature") ?? "";
   if (!process.env.WEBHOOK_SECRET) {
     throw new Error("WEBHOOK_SECRET is not set");
   }
@@ -30,28 +29,23 @@ export async function POST(req: Request) {
   } catch (err) {
     return new Response("Bad Request", { status: 400 });
   }
-  const eventType = msg.type;
 
-  //console.log("eventType", eventType); //trả ra các cái mà bên lúc tạo webhook chọn (user)
+  const eventType = msg.type;
   if (eventType === "user.created") {
-    //create user to database
-    // có cái msg.data => chứa tt
-    // console.log(msg.data);
+    // create user to database
     const { id, username, email_addresses, image_url } = msg.data;
     const user = await createUser({
       username: username!,
       name: username!,
       clerkId: id,
-      email: email_addresses[0].email_address, // đọc ở cái logs trên dashboard
+      email: email_addresses[0].email_address,
       avatar: image_url,
     });
-    // trả ra cho người dùng
     return NextResponse.json({
-      message: "Create user success",
+      message: "OK",
       user,
     });
   }
-  // Rest
 
   return new Response("OK", { status: 200 });
 }
