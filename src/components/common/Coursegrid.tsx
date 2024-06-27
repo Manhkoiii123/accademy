3ae82b8c87b603bb71424ca326258e5d0218dc5b
manhtranduc <manhtranduc0202@gmail.com>
2024-06-27 17:13:04 +0700

    fix bug ui

--- src/components/common/Coursegrid.tsx
+++ src/components/common/Coursegrid.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Coursegrid = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid gap-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 lg:gap-8 mt-8 course-slider">
+    <div className="grid gap-4 lg:grid-cols-2 xl:grid-cols-3  lg:gap-8 mt-8 course-slider">
       {children}
     </div>
   );
